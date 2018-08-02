d65edc5fd8dfbe97b6001740515fe59e71247714
vaalcat <val.cat456@icloud.com>
2018-08-02 18:24:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,28 @@
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
 
+ configure do
+   set :public_folder, 'public'
+   set :views, 'app/views'
+ end
+ 
+   get '/charity_quiz' do
+   erb :charity_quiz
+ end
+
+post '/charity' do
+ @name = params[:username]
+ @answer = params[:q1]  
+ 
+ if @answer == "animal"
+ erb :animal
+ else 
+  erb :human 
+end 
+end 
+
+get '/charity' do 
+erb :charity
+ end
+ 
 end
