40f2222bb02c691863d21b7523fa0d7a84e1f5a2
vaalcat <val.cat456@icloud.com>
2018-08-04 02:06:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,32 @@
 class ApplicationController < Sinatra::Base
 
-
  configure do
    set :public_folder, 'public'
    set :views, 'app/views'
  end
  
-   get '/charity_quiz' do
+   get '/' do
    erb :charity_quiz
  end
 
+get '/animal' do
+ erb :animal
+end
+
+
 post '/charity' do
  @name = params[:username]
- @answer = params[:q1]  
- 
- if @answer == "animal"
- erb :animal
- else 
-  erb :human 
-end 
-end 
+ @answer = params[:q1]
 
-get '/charity' do 
-erb :charity
- end
+if @answer.to_i == 2
+   erb :animal
+ else
+   erb :human
+end
+end
  
+get '/charity' do
+erb :charity
+end
+
 end
